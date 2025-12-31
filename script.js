9e5689b5c6b082fb94db9b7dd8e3226b8bb7790a
Ojas Ankit <ojasankit@gmail.com>
2025-12-31 17:21:13 +0530

    more grid changes

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 const container = document.querySelector("#container")
-let gridArray = []
+const form = document.createElement("form")
 
 
 for(let i = 0; i < 16; i ++) {
-    let gridRow = []
     const row = document.createElement("div")
     row.classList.add(`row${i + 1}`)
 
     for(let j = 0; j < 16; j++) {
         const grid = document.createElement("div")
-        grid.style.width = "100px"
-        grid.style.height = "100px"
-        grid.style.border = "2px solid grey"
+        grid.style.width = "55px"
+        grid.style.height = "55px"
+        grid.style.border = "1px solid grey"
         // grid.style.backgroundColor = "lightgrey"
         
-        grid.addEventListener("mouseover", (e) => {
-            grid.style.backgroundColor = "lightgrey"
+        grid.addEventListener("mousemove", (e) => {
+            grid.style.backgroundColor = "#cacacaff"
+            const x = e.x;
+            const y = e.y;
+            console.log(`X: ${x} Y: ${y}`);
         })
 
-        grid.addEventListener("mouseout", (e) => {
-            grid.style.backgroundColor = "white"
-        })
+        // grid.addEventListener("mouseout", (e) => {
+        //     grid.style.backgroundColor = "white"
+        // })
 
         row.append(grid)
     }
     row.style.display = "flex"
     row.style.justifyContent = "center"
-    gridArray.push(gridRow)
     container.append(row)
 }
